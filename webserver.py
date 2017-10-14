be050d839acadbc00f141e5da81441d65c73a2f7
camille2019 <camilleharris97@gmail.com>
2017-10-14 14:48:33 -0700

    webserver.py

--- webserver.py
+++ webserver.py
@@ -10,3 +10,32 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html') # Render the template located in "templates/index.html"
+
+
+@app.route('/about')
+def about():
+    return render_template('AboutUs.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('ContactUs.html')
+
+@app.route('/blog/8-experiments-in-motivation')
+def motivation():
+    return render_template('8ExperimentsInMotivation.html')
+
+@app.route('/blog/a-mindful-shift-of-focus')
+def focus():
+    return render_template('AMindfulShiftOfFocus.html')
+
+@app.route('/blog/how-to-develop-an-awesome-sense-of-direction')
+def direction():
+    return render_template('HowToDevelopAnAwesomeSenseOfDirection')
+
+@app.route('/blog/training-to-be-a-good-writer')
+def writer():
+    return render_template('TrainingToBeAGoodWriter.html')
+
+@app.route('/blog/what-productivity-systems-wont-solve')
+def productivity():
+    return render_template('WhatProductivitySystemsWontSolve')
