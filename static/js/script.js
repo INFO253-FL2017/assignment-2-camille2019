cc71ab39c24e20b866d54dcc64b2fe50f25c9efa
camille2019 <camilleharris97@gmail.com>
2017-10-14 13:45:10 -0700

    js

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,21 @@
-// script.js
+var m = document.getElementById("#div");
+div.innerHTML = localStorage.curLocation;
 
-console.log("js/script.js has been loaded!");
+contactUs.getElementById("div").textContent = 'This is dynamically added text';
+/* Run code on submit button push */
+var contactUs = document.getElementById("contactUs");
+contactUs.addEventListener("submit", function(event) {
+	var name = contactUs.elements.namedItem("name").value;
+	var subject = contactUs.elements.namedItem("subject").value;
+  var message = contactUs.elements.namedItem("message").value;
+
+	localStorage.curLocation = name + ": " + subject + ":" + message;
+
+	var m = document.getElementById("div");
+	m.innerHTML = localStorage.curLocation;
+	m.classList.add("highlight");
+
+	/* This stops the usual function of "submit" which is to send data
+	to another server */
+	event.preventDefault();
+})
