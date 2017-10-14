46480b488de1388a609bdd91a1028a72cc775a1d
camille2019 <camilleharris97@gmail.com>
2017-10-14 13:03:08 -0700

    proj 2 update

--- webserver.py
+++ webserver.py
@@ -10,10 +10,34 @@ from flask import Flask, render_template
 # "static folder"
 app = Flask(__name__,static_url_path="/static")
 
-@app.route('/')
-def hello_world():
-    """
-    If someone goes to the root of your website (i.e. http://localhost:5000/), run this function. The string that this
-    returns will be sent to the browser
-    """
+@app.route('/index')
+def index():
     return render_template("index.html") # Render the template located in "templates/index.html"
+
+@app.route('/about')
+def about():
+    return render_template('About Us.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('Contact Us.html')
+
+@app.route('/blog/8-experiments-in-motivation')
+def motivation():
+    return render_template('8 Experiments in Motivation.html')
+
+@app.route('/blog/a-mindful-shift-of-focus')
+def focus():
+    return render_template('A Mindful Shift of Focus.html')
+
+@app.route('/blog/how-to-develop-an-awesome-sense-of-direction')
+def direction():
+    return render_template('How to Develop an Awesome Sense of Direction')
+
+@app.route('/blog/training-to-be-a-good-writer')
+def writer():
+    return render_template('Training to be a Good Writer.html')
+
+@app.route('/blog/what-productivity-systems-wont-solve')
+def productivity():
+    return render_template('What Productivity Systems Won't Solve')
