fade3fe3f91c4c71a318202d34573f8589a4eaa2
camille2019 <camilleharris97@gmail.com>
2017-10-14 14:40:56 -0700

    render template

--- webserver.py
+++ webserver.py
@@ -7,6 +7,6 @@ File that is the central location of code for your webserver.
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-@app.route('/index')
+@app.route('/')
 def index():
-    return render_template("index.html") # Render the template located in "templates/index.html"
+    return render_template('index.html') # Render the template located in "templates/index.html"
