48b21612d1b0d6cf25bdc3d95e8971fcbf892d11
camille2019 <camilleharris97@gmail.com>
2017-10-14 14:20:30 -0700

    webserver

--- webserver.py
+++ webserver.py
@@ -4,40 +4,10 @@ webserver.py
 File that is the central location of code for your webserver.
 """
 
-from flask import Flask, render_template
-
-# Create application, and point static path (where static resources like images, css, and js files are stored) to the
-# "static folder"
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/index')
-def index():
-    return render_template("index.html") # Render the template located in "templates/index.html"
-
-@app.route('/about')
-def about():
-    return render_template('About Us.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('Contact Us.html')
-
-@app.route('/blog/8-experiments-in-motivation')
-def motivation():
-    return render_template('8 Experiments in Motivation.html')
-
-@app.route('/blog/a-mindful-shift-of-focus')
-def focus():
-    return render_template('A Mindful Shift of Focus.html')
-
-@app.route('/blog/how-to-develop-an-awesome-sense-of-direction')
-def direction():
-    return render_template('How to Develop an Awesome Sense of Direction')
-
-@app.route('/blog/training-to-be-a-good-writer')
-def writer():
-    return render_template('Training to be a Good Writer.html')
-
-@app.route('/blog/what-productivity-systems-wont-solve')
-def productivity():
-    return render_template('What Productivity Systems Wont Solve')
+@app.route('/')
+def hello_world():
+  return "<h1>Hello World!</h1>"
+					
